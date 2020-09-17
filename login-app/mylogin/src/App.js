902fb6832edba590bbc183526f03e01a1861d0e8
marahmbawwab <marah.bawwab@gmail.com>
2020-09-17 21:03:41 +0300

    "last"

--- login-app/mylogin/src/App.js
+++ login-app/mylogin/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Myform extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {username:null ,
+      password:null 
+    }
+    this.handleuser=this.handleuser.bind(this);
+  }
+  handleuser(event){
+      this.setState({username:event.target.value});
+  }
+ handlepass=(event)=>{
+   //if(event.target.name==="myuser"){
+     this.setState({password:event.target.value});
+ }
+  handleClick=()=>{
+    var str1 = "User Name:" + this.state.username ;
+    var str2 = " , Password:"+this.state.password ;
+    var res = str1.concat(str2);
+    return alert (res);
+  }
+  render(){
+    return (
+      <div style={{margin : "50px", display:"flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+      <div style={{margin:"5px" ,display:"flex", flexDirection:"row" }}>
+        <label for="user" style={{width:"100px"}}><b>User Name:</b></label>
+        <input type="text"  name="myuser" onChange={this.handleuser} style={{borderRadius:"7px",height:"20px"}}/>
+      </div>
+      <div style={{margin:"5px" ,display:"flex", flexDirection:"row" }}>
+        <label for="pass" style={{width:"100px"}}><b>Password :</b></label>
+        <input type="password" onChange={this.handlepass}  name="mypass" style={{borderRadius:"7px",height:"20px"}}/>
+      </div>
+      <div style={{alignSelf:"flex-start"}}>
+        <button style={{borderRadius:"8px",height:"30px",width:"60px"}} onClick={this.handleClick}>LOGIN</button>
+      </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+class Page extends React.Component {
+  
+  render(){
+    return (
+      <div style={{height:"100vh",backgroundColor:"rgb(111,111,146)",display:"flex",flexDirection:"column"/*justifyContent:"center",alignItems:"center"*/}} >
+     <p style={{textAlign:"center",color:"rgb(255,226,226)",fontSize:"50px",fontWeight:"bold"}}>Welcome To Our Website</p>
+     <div style={{alignSelf:"center",backgroundColor:"white",borderRadius:"20px",width:"600px",height:"250px",flexDirection:"row",display:"flex"}}>
+      <img  src={require('./rabbit.jpg')}  alt="no-display" width="100px" height="100px" style={{borderRadius:"50%",margin:"50px"}}/>
+      <Myform />
+     </div>
+     </div>
+    );
+    }
+  }
+
+export default Page;
+
